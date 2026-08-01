b43122e774398dd6d2bcfa130b319b227fed3987
DESKTOP-BUF33D0\Luz <stepanovoleg.dev@gmail.com>
2026-08-01 22:44:06 +0300

    Update to 5.8

--- Source/UE_MinimalEditor.Target.cs
+++ Source/UE_MinimalEditor.Target.cs
@@ -8,8 +8,8 @@ public class UE_MinimalEditorTarget : TargetRules
 	public UE_MinimalEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("UE_Minimal");
 	}
 }
